6826c39977b330b3dfc595f288d420ea69a3aaef
Sevansoft <github@sevansoft.co.uk>
2015-03-11 23:16:45 +0000

    batch file tweak, clear cache on each test, superfluous cache method removed, loggin in cache (debug mode)

--- VS2013/Sevansoft.CacheTest.Caching/Cache.vb
+++ VS2013/Sevansoft.CacheTest.Caching/Cache.vb
@@ -14,68 +14,63 @@ Imports System.Runtime.Caching
 Namespace Sevansoft.CacheTest.Caching
     <DebuggerStepThrough>
     Public NotInheritable Class Cache
+
         Private Sub New()
 
         End Sub
 
         Public Shared Sub ClearCache()
             Dim cacheKeys As IEnumerable(Of String) = InternalGetCache().Select(Function(kvp) kvp.Key)
-            LogEvent("Clearing cache")
+
             For Each cacheKey As String In cacheKeys
                 InternalGetCache().Remove(cacheKey)
             Next cacheKey
-            LogEvent("Cleared cache")
         End Sub
 
         Public Shared Function [Get](Of T As Class)(ByVal cacheKey As String,
                                                     ByVal GetItemCallBack As Func(Of T)) As T
-            Return [Get](cacheKey, GetItemCallBack, New TimeSpan(24, 0, 0))
-        End Function
-
-        Public Shared Function [Get](Of T As Class)(ByVal cacheKey As String,
-                                                    ByVal GetItemCallBack As Func(Of T),
-                                                    ByVal slidingExpiration As TimeSpan) As T
             Dim result As T = TryCast(InternalGetCache().Get(cacheKey), T)
 
             If result Is Nothing Then
-                LogEvent("Item key {0} is not cached", cacheKey)
                 result = GetItemCallBack()
                 If result IsNot Nothing Then
-                    InternalInsertToCache(cacheKey, result, slidingExpiration)
+                    InternalInsertToCache(cacheKey, result)
                 End If
-            Else
-                LogEvent("Item key {0} is cached", cacheKey)
+
             End If
 
             Return result
         End Function
 
-        Private Shared Sub InternalInsertToCache(ByVal Key As String,
+
+        Private Shared Sub InternalInsertToCache(ByVal Key As String, _
                                                  ByVal value As Object)
-            InternalInsertToCache(Key, value, New TimeSpan(24, 0, 0))
+            InternalInsertToCache(Key, value, New TimeSpan(24, 0, 0), CacheItemPriority.Default)
         End Sub
 
-        Private Shared Sub InternalInsertToCache(ByVal Key As String,
-                                                 ByVal value As Object,
-                                                 ByVal slidingExpiration As TimeSpan)
-            InternalInsertToCache(Key, value, slidingExpiration, CacheItemPriority.Default)
-        End Sub
 
-        Private Shared Sub InternalInsertToCache(ByVal Key As String,
-                                                 ByVal value As Object,
-                                                 ByVal slidingExpiration As TimeSpan,
-                                                 ByVal priority As CacheItemPriority)
+        Private Shared Sub InternalInsertToCache(ByVal Key As String, _
+                                                ByVal value As Object, _
+                                                ByVal slidingExpiration As TimeSpan, _
+                                                ByVal priority As CacheItemPriority)
             InternalInsertToCache(Key, value, ObjectCache.InfiniteAbsoluteExpiration, slidingExpiration, priority, Nothing)
         End Sub
 
-        Private Shared Sub InternalInsertToCache(ByVal Key As String,
-                                                 ByVal value As Object,
-                                                 ByVal absoluteExpiration As DateTimeOffset,
-                                                 ByVal slidingExpiration As TimeSpan,
-                                                 ByVal priority As CacheItemPriority,
+        Private Shared Sub InternalInsertToCache(ByVal Key As String, _
+                                                 ByVal value As Object, _
+                                                 ByVal priority As CacheItemPriority)
+            InternalInsertToCache(Key, value, ObjectCache.InfiniteAbsoluteExpiration, New TimeSpan(24, 0, 0), priority, Nothing)
+        End Sub
+
+        Private Shared Sub InternalInsertToCache(ByVal Key As String, _
+                                                 ByVal value As Object, _
+                                                 ByVal absoluteExpiration As DateTimeOffset, _
+                                                 ByVal slidingExpiration As TimeSpan, _
+                                                 ByVal priority As CacheItemPriority, _
                                                  ByVal onRemoveCallback As CacheEntryRemovedCallback)
 
             Dim cacheItemPolicy As CacheItemPolicy = New CacheItemPolicy()
+
 
             cacheItemPolicy.AbsoluteExpiration = absoluteExpiration
             cacheItemPolicy.Priority = priority
@@ -83,26 +78,10 @@ Namespace Sevansoft.CacheTest.Caching
             cacheItemPolicy.RemovedCallback = onRemoveCallback
 
             InternalGetCache.Add(Key, value, cacheItemPolicy)
-            LogEvent("Add item key {0} to cache", Key)
         End Sub
 
         Private Shared Function InternalGetCache() As ObjectCache
             Return MemoryCache.Default
         End Function
-
-        <Conditional("DEBUG")>
-        Private Shared Sub LogEvent(ByVal format As String, ByVal ParamArray args As Object())
-            If Not EventLog.SourceExists("Sevansoft.CacheTest.Caching") Then
-                EventLog.CreateEventSource("Sevansoft.CacheTest.Caching", "Application")
-            End If
-            Dim log As EventLog = New EventLog("Application", Environment.MachineName, "Sevansoft.CacheTest.Caching")
-            Dim eventMessage As String = format
-            If args IsNot Nothing Then
-                eventMessage = String.Format(format, args)
-            End If
-
-            log.WriteEntry(eventMessage, EventLogEntryType.Information)
-            Console.WriteLine(eventMessage)
-        End Sub
     End Class
 End Namespace
--- VS2013/Sevansoft.CacheTest.Client.ConsoleApp/generate proxy.bat
+++ VS2013/Sevansoft.CacheTest.Client.ConsoleApp/generate proxy.bat
@@ -1,5 +1,12 @@
-rem "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" /language:vb /out:Proxy.vb /config:proxy.config http://sevansoft-wcfcache.azurewebsites.net/ProductService.Svc?wsdl http://sevansoft-wcfcache.azurewebsites.net/CountryInformationService.svc?wsdl
-rem pause
+@ECHO OFF
 
+IF "%Program Files(x86)" == "" GOTO X86ONLY
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" /language:vb /out:Proxy.vb /config:proxy.config http://sevansoft-wcfcache.azurewebsites.net/ProductService.Svc?wsdl http://sevansoft-wcfcache.azurewebsites.net/CountryInformationService.svc?wsdl
+GOTO END
+
+:X86ONLY
 "C:\Program Files\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" /language:vb /out:Proxy.vb /config:proxy.config http://sevansoft-wcfcache.azurewebsites.net/ProductService.Svc?wsdl http://sevansoft-wcfcache.azurewebsites.net/CountryInformationService.svc?wsdl
+GOTO END
+
+:END
 pause
