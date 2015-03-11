82f8293f46134df8da5fd500c9f34dfdea90c966
Sevansoft <github@sevansoft.co.uk>
2015-03-11 23:54:08 +0000

    logging and additional expiry test

--- VS2013/Sevansoft.CacheTest.Caching.Tests/CacheTests.vb
+++ VS2013/Sevansoft.CacheTest.Caching.Tests/CacheTests.vb
@@ -4,6 +4,7 @@ Option Strict On
 #End Region
 
 Imports System
+Imports System.Runtime.Caching
 Imports System.Threading
 
 Imports NUnit.Framework
@@ -13,19 +14,45 @@ Imports Sevansoft.CacheTest.Caching
 Namespace Sevansoft.CacheTest.Caching.Tests
     <TestFixture>
     Public Class CacheTests
+        <SetUp>
+        Public Sub Setup()
+            Cache.ClearCache()
+        End Sub
+
         <Test>
         Public Sub ShouldCacheItem()
             Dim value As DateTime = DateTime.Now
-            Thread.Sleep(1000)
+            Thread.Sleep(500)
 
             Dim firstItem As CachedItem = Cache.Get(Of CachedItem)("cacheKey", Function() GetTestCacheItem(value))
 
-            Thread.Sleep(1000)
+            Thread.Sleep(500)
             Dim secondItem As CachedItem = Cache.Get(Of CachedItem)("cacheKey", Function() GetTestCacheItem(DateTime.Now))
 
             Assert.AreEqual(value, firstItem.Value, "firstItem.Value is invalid")
 
             Assert.AreEqual(firstItem.Text, secondItem.Text, "secondItem.Text is invalid")
+        End Sub
+
+        <Test>
+        Public Sub CacheItemShouldExpire()
+            Dim value As DateTime = DateTime.Now
+
+            Dim firstItem As CachedItem = Cache.Get(Of CachedItem)("cacheKey", Function() GetTestCacheItem(value), New TimeSpan(0, 0, 2))
+
+            Thread.Sleep(500)
+            Dim secondItem As CachedItem = Cache.Get(Of CachedItem)("cacheKey", Function() GetTestCacheItem(DateTime.Now), New TimeSpan(0, 0, 2))
+
+            Assert.AreEqual(value, firstItem.Value, "firstItem.Value is invalid")
+
+            Assert.AreEqual(firstItem.Text, secondItem.Text, "secondItem.Text is invalid")
+
+            Thread.Sleep(2100)
+            Dim thirdItem As CachedItem = Cache.Get(Of CachedItem)("cacheKey", Function() GetTestCacheItem(DateTime.Now), New TimeSpan(0, 0, 2))
+
+            Assert.AreNotEqual(value, thirdItem.Value, "firstItem.Value is invalid")
+
+            Assert.AreNotEqual(firstItem.Text, thirdItem.Text, "secondItem.Text is invalid")
         End Sub
 
         Private Function GetTestCacheItem() As CachedItem
