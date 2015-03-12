3622b10a914d83a23ecbbd0e30faf74e197b7e33
Sevansoft <github@sevansoft.co.uk>
2015-03-12 00:10:26 +0000

    batch file correction

--- VS2013/Sevansoft.CacheTest.Client.ConsoleApp/generate proxy.bat
+++ VS2013/Sevansoft.CacheTest.Client.ConsoleApp/generate proxy.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-IF "%Program Files(x86)" == "" GOTO X86ONLY
+IF "%Program Files(x86)%" == "" GOTO X86ONLY
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\svcutil.exe" /language:vb /out:Proxy.vb /config:proxy.config http://sevansoft-wcfcache.azurewebsites.net/ProductService.Svc?wsdl http://sevansoft-wcfcache.azurewebsites.net/CountryInformationService.svc?wsdl
 GOTO END
 
