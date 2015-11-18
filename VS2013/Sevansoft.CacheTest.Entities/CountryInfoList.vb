e600ab0497c7850d0562defd8bec6fc1e7a7fa0a
Sevansoft <github@sevansoft.co.uk>
2015-11-18 19:43:36 +0000

    [Date] Removed

--- VS2013/Sevansoft.CacheTest.Entities/CountryInfoList.vb
+++ VS2013/Sevansoft.CacheTest.Entities/CountryInfoList.vb
@@ -16,10 +16,6 @@ Namespace Sevansoft.CacheTest.Entities
     Public Class CountryInfoList
         Inherits List(Of CountryInfo)
 
-        Public Sub New()
-            Me.Date = DateTime.Now
-        End Sub
-
         Public Shared Function LoadList(ByVal path As FileSystemInfo) As CountryInfoList
             Dim result As CountryInfoList = New CountryInfoList()
 
@@ -37,10 +33,6 @@ Namespace Sevansoft.CacheTest.Entities
 
             Return result
         End Function
-
-        <DataMember>
-        Public Property [Date] As DateTime
-
     End Class
 
 End Namespace
